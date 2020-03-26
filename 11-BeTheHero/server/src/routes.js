e7d2c8d13fb0dd7fb33be9511cc644e10f460e8b
Manfrinmm <matheus_mm123@hotmail.com>
2020-03-26 00:54:20 -0300

    Edit route incidents by ONG

--- 11-BeTheHero/server/src/routes.js
+++ 11-BeTheHero/server/src/routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import IncidentController from "./app/controllers/IncidentController";
 import OngController from "./app/controllers/OngController";
-import ProfileController from "./app/controllers/ProfileController";
+import OngIncidentsController from "./app/controllers/OngIncidentsController";
 import SessionController from "./app/controllers/SessionController";
 
 const routes = Router();
@@ -11,11 +11,10 @@ routes.post("/sessions", SessionController.store);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.store);
+routes.get("/ongs/incidents", OngIncidentsController.index);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.store);
 routes.delete("/incidents/:id", IncidentController.delete);
-
-routes.get("/profile", ProfileController.index);
 
 export default routes;
