7d923768e119efa234394b84149f39f499baf41c
Manfrinmm <matheus_mm123@hotmail.com>
2020-03-28 18:15:20 -0300

    Add validation form & Database & Insert, Delete and Search

--- WorkshopDev/web/scripts.js
+++ WorkshopDev/web/scripts.js
@@ -5,3 +5,33 @@ function onOff() {
 
   document.querySelector("#modal").classList.toggle("AddScroll");
 }
+
+function checkFields(event) {
+  const fieldsToCheck = [
+    "title",
+    "category",
+    "img_url",
+    "description",
+    "idea_url"
+  ];
+
+  fieldsToCheck.forEach(field => {
+    const fieldSelected = event.target[field];
+    const span = fieldSelected.parentElement.lastElementChild;
+
+    const isString = typeof fieldSelected.value === "string";
+
+    const isEmpty = fieldSelected.value.trim() === "";
+
+    if (isString && !isEmpty) {
+      fieldSelected.classList.remove("error");
+      span.classList.remove("error");
+    } else {
+      event.preventDefault();
+
+      fieldSelected.classList.add("error");
+
+      span.classList.add("error");
+    }
+  });
+}
