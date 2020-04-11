34a1e9206c423d7af01bb902164acf3e8bd1300e
Manfrinmm <matheus_mm123@hotmail.com>
2020-04-11 13:23:17 -0300

    Remove static host

--- 9-TinDev/backend/src/server.js
+++ 9-TinDev/backend/src/server.js
@@ -2,4 +2,6 @@ require("dotenv/config");
 
 const App = require("./App");
 
-App.listen(process.env.APP_PORT, process.env.APP_HOST);
+App.listen(process.env.APP_PORT, () => {
+  console.log(` server started on port ${process.env.APP_PORT}`);
+});
